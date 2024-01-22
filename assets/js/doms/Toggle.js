675a418efca2f37fa8dd29ef8ff2520fa2336776
drew-chase <dcmanproductions@gmail.com>
2024-01-22 10:32:47 -0500

    Refactor Toggle class to accept name and value parameters

--- assets/js/doms/Toggle.js
+++ assets/js/doms/Toggle.js
@@ -1,12 +1,18 @@
 export default class Toggle extends HTMLElement {
-    constructor() {
+    constructor(name, value) {
         super();
-        this.value = false;
+        this.name = name;
+        this.value = value;
+        this.setAttribute("name", name);
+        this.setAttribute("value", value);
         this.$ = $(this);
+        this.$.html(name);
     }
 
     connectedCallback() {
-        this.value = this.getAttribute("value");
+        if (this.name != undefined) this.setAttribute("name", this.name);
+        if (this.value != undefined) this.setAttribute("value", this.value);
+        else this.value = this.getAttribute("value") == true;
         this.$.on("click", (e) => {
             this.value = !this.value;
             this.setAttribute("value", this.value);
