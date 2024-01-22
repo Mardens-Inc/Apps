bf31ff7c2b59e3f468ec886a45750d2626f0205f
drew-chase <dcmanproductions@gmail.com>
2024-01-22 10:32:30 -0500

    Refactor Dropdown class to improve functionality and readability

--- assets/js/doms/Dropdown.js
+++ assets/js/doms/Dropdown.js
@@ -1,7 +1,6 @@
 import DropdownOption from "./DropdownOption.js";
 
 export default class Dropdown extends HTMLElement {
-
     /**
      * Creates a new Dropdown object.
      * @param {string} label The label of the dropdown.
@@ -11,14 +10,41 @@ export default class Dropdown extends HTMLElement {
         super();
         this.label = label;
         this.options = options;
-        this.value = null;
+        this.setAttribute("label", label);
+        this.value = this.getAttribute("value");
         this.$ = $(this);
+        if (this.$.find(".dropdown-items").length == 0) {
+            const items = $("<div class='dropdown-items'></div>");
+            items.append(this.options);
+            this.$.append(items);
+        }
     }
 
-
     connectedCallback() {
-        this.label = this.getAttribute('label');
-        this.options = this.children;
+        this.setAttribute("tabindex", "-1");
+        if (this.label == undefined && this.$.find(".name").length != 0) this.label = this.$.find(".name").html();
+        if (this.label == undefined) this.label = this.getAttribute("label");
+        if (this.label != undefined) this.$.prepend(`<span class="name">${this.label}</span>`);
+        if (this.value != undefined) this.setAttribute("value", this.value);
+
+        this.options = this.$.find("dropdown-option");
+
+        if (this.$.find("dropdown-option[selected]").length != 0) {
+            this.selectedOption = this.$.find("dropdown-option[selected]")[0];
+            this.selectOption(this.selectedOption);
+        }
+
+        const valueLabel = this.$.find(".value");
+        if (valueLabel.length == 0) {
+            this.$.append($(`<span class="value">${this.selectedOption == undefined ? "" : this.selectedOption.label}</span>`));
+        } else {
+            this.$.find(".value").html(this.selectedOption == undefined ? "" : this.selectedOption.label);
+        }
+
+        this.$.find("dropdown-option").on("click", (e) => {
+            this.selectOption(e.target);
+            this.blur();
+        });
     }
     /**
      * Adds an option to the dropdown.
@@ -37,6 +63,22 @@ export default class Dropdown extends HTMLElement {
         this.options.splice(this.options.indexOf(option), 1);
         this.removeChild(option);
     }
+    selectOption(option) {
+        this.value = option.value;
+        this.setAttribute("value", option.value);
+        this.$.find("dropdown-option").attr("selected", null);
+        option.select();
+        const valueLabel = this.$.find(".value");
+        if (valueLabel.length == 0) {
+            this.$.append($(`<span class="value">${option.label}</span>`));
+        } else {
+            this.$.find(".value").html(option.label);
+        }
+        this.$.trigger("change", [{ value: option.value }]);
+    }
+    selectIndex(index) {
+        this.selectOption(this.options[index]);
+    }
 }
 
-customElements.define('drop-down', Dropdown);
+customElements.define("drop-down", Dropdown);
