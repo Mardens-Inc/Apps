53a76d2be35d0404f800cb348183cc1f997117a0
drew-chase <dcmanproductions@gmail.com>
2024-01-22 10:32:41 -0500

    Refactor DropdownOption class to improve readability and maintainability

--- assets/js/doms/DropdownOption.js
+++ assets/js/doms/DropdownOption.js
@@ -1,26 +1,34 @@
 export default class DropdownOption extends HTMLElement {
     constructor(label, value, selected = false) {
         super();
-        this.label = label;
-        this.value = value;
-        this.selected = selected;
         this.$ = $(this);
-        this.setAttribute("value", value);
-        if (selected) {
-            this.setAttribute("selected", "");
+        if (label != undefined) {
+            this.label = label;
+            this.$.append(`${label}`);
         }
-        this.$.append(`<label>${label}</label>`)
+        if (value != undefined) this.value = value;
+        if (selected != undefined) {
+            this.selected = selected;
+            if (selected) {
+                this.setAttribute("selected", "");
+            }
+        }
+        if (value != undefined) this.setAttribute("value", value);
     }
 
     connectedCallback() {
-        this.label = this.$.find("label").html();
+        this.label = this.$.html();
         this.value = this.getAttribute("value");
-        this.selected = this.getAttribute("selected");
+        this.selected = this.getAttribute("selected") != undefined;
     }
 
     select() {
         this.selected = true;
-        this.classList.add("selected");
+        this.setAttribute("selected", "");
+    }
+    deselect() {
+        this.selected = false;
+        this.removeAttribute("selected");
     }
 }
 
