a5bbc36999571f365b3745abc77fe861533fd681
drew-chase <dcmanproductions@gmail.com>
2024-01-23 09:28:39 -0500

    Refactor buildAddAppOptionsFromTemplate function

--- assets/js/apps-list.js
+++ assets/js/apps-list.js
@@ -8,7 +8,7 @@ $("dialog#add-item-modal drop-down#template").on("change", (_, data) => {
     buildAddAppOptionsFromTemplate(data.value);
 });
 
-$("dialog#add-item-modal drop-down#template dropdown-option")[0].click();
+$("dialog#add-item-modal drop-down#template dropdown-option")[1].click();
 /**
  *
  * @param {string} template The template to build the options from.
@@ -21,6 +21,10 @@ async function buildAddAppOptionsFromTemplate(template) {
     templateOptions.append($(`<p style="text-align: center; font-size: 1.5rem; margin-top: 10px; margin-bottom: 1rem;">${json["description"]}</p>`));
 
     for (const option of json["options"]) {
+        const id = option.name.toLowerCase().replace(/[^a-z]/g, "-");
+        console.log(id);
+        let element;
+
         switch (option.type) {
             case "select":
                 const options = option.options.map((o) => new DropdownOption(o.name, o.value, o.value == option.default));
@@ -29,87 +33,110 @@ async function buildAddAppOptionsFromTemplate(template) {
                 templateOptions.append(dropdown);
                 break;
             case "boolean":
-                const toggle = new Toggle(option.name, option.default);
-                toggle.title = option.description;
-                templateOptions.append(toggle);
+                element = new Toggle(option.name, option.default);
+                element.title = option.description;
+
+                // check if any other options have a condition for this option
+                const conditionals = json["options"].filter((o) => o.condition && o.condition == option.name);
+                const update = (value) => {
+                    if (value) {
+                        conditionals.forEach((o) => {
+                            const element = o.element;
+                            if (element) {
+                                element.css("display", "block");
+                            }
+                        });
+                    } else {
+                        conditionals.forEach((o) => {
+                            const element = o.element;
+                            if (element) {
+                                element.css("display", "none");
+                            }
+                        });
+                    }
+                };
+                $(element).on("toggle", (_, e) => {
+                    update(e.value);
+                });
+                $(document).on("finish-building-options", () => {
+                    update(element.value);
+                });
+
                 break;
             case "text":
-                const floatingLabel = $(`<div class="floating-input col"></div>`);
+                element = $(`<div class="floating-input col" id="${id}"></div>`);
                 const label = $(`<label for="${option.name}">${option.name}</label>`);
                 const text = $(`<input type="text" placeholder="${option.description}" name="${option.name}" value="${option.default}">`);
                 text.title = option.description;
-                floatingLabel.append(text);
-                floatingLabel.append(label);
-                templateOptions.append(floatingLabel);
+                element.append(text);
+                element.append(label);
                 break;
             case "number":
-                const floatingLabel2 = $(`<div class="floating-input col"></div>`);
+                element = $(`<div class="floating-input col"></div>`);
                 const label2 = $(`<label for="${option.name}">${option.name}</label>`);
                 const text2 = $(`<input type="number" placeholder="${option.description}" name="${option.name}" value="${option.default}">`);
                 text2.title = option.description;
-                floatingLabel2.append(text2);
-                floatingLabel2.append(label2);
-                templateOptions.append(floatingLabel2);
+                element.append(text2);
+                element.append(label2);
                 break;
             case "color":
-                const floatingLabel3 = $(`<div class="color-input center vertical row"></div>`);
+                element = $(`<div class="color-input center vertical row"></div>`);
                 const label3 = $(`<label for="${option.name}" class='fill'>${option.name}</label>`);
                 const text3 = $(`<input type="color" placeholder="${option.description}" name="${option.name}" value="${option.default}">`);
-                floatingLabel3.attr("title", option.description);
-                floatingLabel3.append(label3);
-                floatingLabel3.append(text3);
-                templateOptions.append(floatingLabel3);
+                element.attr("title", option.description);
+                element.append(label3);
+                element.append(text3);
                 break;
             case "date":
-                const floatingLabel4 = $(`<div class="floating-input col"></div>`);
+                element = $(`<div class="floating-input col"></div>`);
                 const label4 = $(`<label for="${option.name}">${option.name}</label>`);
                 const text4 = $(`<input type="date" placeholder="${option.description}" name="${option.name}" value="${option.default}">`);
                 text4.title = option.description;
-                floatingLabel4.append(text4);
-                floatingLabel4.append(label4);
-                templateOptions.append(floatingLabel4);
+                element.append(text4);
+                element.append(label4);
                 break;
             case "time":
-                const floatingLabel5 = $(`<div class="floating-input col"></div>`);
+                element = $(`<div class="floating-input col"></div>`);
                 const label5 = $(`<label for="${option.name}">${option.name}</label>`);
                 const text5 = $(`<input type="time" placeholder="${option.description}" name="${option.name}" value="${option.default}">`);
                 text5.title = option.description;
-                floatingLabel5.append(text5);
-                floatingLabel5.append(label5);
-                templateOptions.append(floatingLabel5);
+                element.append(text5);
+                element.append(label5);
                 break;
             case "datetime":
-                const floatingLabel6 = $(`<div class="floating-input col"></div>`);
+                element = $(`<div class="floating-input col"></div>`);
                 const label6 = $(`<label for="${option.name}">${option.name}</label>`);
                 const text6 = $(`<input type="datetime-local" placeholder="${option.description}" name="${option.name}" value="${option.default}">`);
                 text6.title = option.description;
-                floatingLabel6.append(text6);
-                floatingLabel6.append(label6);
-                templateOptions.append(floatingLabel6);
+                element.append(text6);
+                element.append(label6);
                 break;
             case "textarea":
-                const floatingLabel7 = $(`<div class="floating-input col"></div>`);
+                element = $(`<div class="floating-input col"></div>`);
                 const label7 = $(`<label for="${option.name}">${option.name}</label>`);
                 const text7 = $(`<textarea type="text" placeholder="${option.description}" name="${option.name}">${option.default}</textarea>`);
                 text7.title = option.description;
-                floatingLabel7.append(text7);
-                floatingLabel7.append(label7);
-                templateOptions.append(floatingLabel7);
+                element.append(text7);
+                element.append(label7);
                 break;
 
             case "file":
-                const fileInput = $(`<file-input name="${option.name}" description="${option.description}" extensions="${option.extensions.join(",")}" multiple="${option.multiple}" default="${option.default}"></file-input>`);
-                templateOptions.append(fileInput);
+                element = $(`<file-input name="${option.name}" description="${option.description}" extensions="${option.extensions.join(",")}" multiple="${option.multiple}" default="${option.default}"></file-input>`);
                 break;
             case "array":
-                const arrayInput = $(`<array-input name="${option.name}" description="${option.description}" default="${option.default.join(",")}"></array-input>`);
-                templateOptions.append(arrayInput);
+                element = $(`<array-input name="${option.name}" description="${option.description}" default="${option.default.join(",")}"></array-input>`);
             default:
                 break;
         }
+
+        option.element = element;
+
+        templateOptions.append(element);
     }
 
-    templateOptions.append($("<div class='row center vertical fill'><button id='add-item' type='submit' style='margin: auto;width: 200px;'>Add</button></div>"));
+    templateOptions.append($("<div class='row center vertical fill' style='margin-top: 1rem;'><button id='add-item' type='submit' style='margin: auto;width: 200px;'>Add</button></div>"));
+
+    $(document).trigger("finish-building-options");
 }
 
 async function getTemplateJson(template) {
