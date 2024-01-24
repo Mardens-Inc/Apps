e2fd66c04c869fc39fb0134a404c3e6dd69263c6
drew-chase <dcmanproductions@gmail.com>
2024-01-24 12:17:50 -0500

    Add FileMaker API integration and retrieve row names

--- api/filemaker/databases/layouts/rows/index.php
+++ api/filemaker/databases/layouts/rows/index.php
@@ -1,1 +1,9 @@
 <?php
+require_once($_SERVER["DOCUMENT_ROOT"] . "/assets/php/FMUtil.inc.php");
+header("Content-Type: application/json");
+
+$config = json_decode(file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/config.json"), true);
+$username = $config["filemaker"]["username"];
+$password = $config["filemaker"]["password"];
+$fm = new FileMaker($username, $password, $_GET["database"], $_GET["layout"]);
+die(json_encode($fm->getRowNames()));
