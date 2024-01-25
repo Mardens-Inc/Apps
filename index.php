e8faae7852edf748c27fe31f062ab9ebf8872ba9
drew-chase <dcmanproductions@gmail.com>
2024-01-25 13:44:55 -0500

    Improve code readability in `index.php`
    Adjusted indentation and removed excess white spaces in `index.php`. This improvement increased the code's legibility and ease of navigation, aiding future updates and maintenance. The changes did not affect the functionality of the code.

--- index.php
+++ index.php
@@ -39,7 +39,6 @@ if (!file_exists($file) || $last_modified >= filemtime($file)) {
 
     <title>Login - Marden's Surplus & Salvage</title>
 
-
     <link rel="stylesheet" href="/assets/css/main.min.css?version=1">
     <link rel="stylesheet" href="/assets/css/login.min.css?version=1">
     <link rel="stylesheet" href="/assets/css/table.min.css?version=1">
@@ -47,12 +46,8 @@ if (!file_exists($file) || $last_modified >= filemtime($file)) {
     <link rel="stylesheet" href="/assets/css/inputs.min.css?version=1">
 
     <!-- Favicon -->
-    <link rel="icon"
-          href="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_32,h_32/https://www.mardens.com/wp-content/uploads/2019/03/cropped-Mardens-Favicon-1-32x32.png"
-          sizes="32x32">
-    <link rel="icon"
-          href="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_192,h_192/https://www.mardens.com/wp-content/uploads/2019/03/cropped-Mardens-Favicon-1-192x192.png"
-          sizes="192x192">
+    <link rel="icon" href="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_32,h_32/https://www.mardens.com/wp-content/uploads/2019/03/cropped-Mardens-Favicon-1-32x32.png" sizes="32x32">
+    <link rel="icon" href="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_192,h_192/https://www.mardens.com/wp-content/uploads/2019/03/cropped-Mardens-Favicon-1-192x192.png" sizes="192x192">
 
 
     <!-- Font Awesome -->
@@ -64,8 +59,7 @@ if (!file_exists($file) || $last_modified >= filemtime($file)) {
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 </head>
 
